383d396bae8f2072268d36c3a5f60328702a5169
briannaraphino <44124773+briannaraphino@users.noreply.github.com>
2018-10-13 18:50:27 -0400

    Add files via upload

--- Checkwin_2.m
+++ Checkwin_2.m
@@ -4,20 +4,18 @@ for i = 1:3
     if sum(Dboard(i,:)==P1const*ones (1,3))== 3
        
         P1win = 1;
-        disp ('Player 1 wins')
+        fprintf ('%s wins',P1name)
     end
      
-
 
 % Check columns for P1 win
 
     if sum(Dboard(:,1)==P1const*ones (1,3))== 3
        
         P1win = 1;
-        disp ('Player 1 wins')
+       fprintf ('%s wins', P1name)
     end
     
-
 
 
 % Check row for P2 win
@@ -25,7 +23,7 @@ for i = 1:3
     if sum(Dboard(i,:)==P2const*ones (1,3))== 3
        
         P2win = 1;
-        disp ('Player 2 wins')
+        fprintf ('%s wins',P2name)
     end
      
 
@@ -35,29 +33,29 @@ for i = 1:3
     if sum(Dboard(:,1)==P2const*ones (1,3))== 3
        
         P2win = 1;
-        disp ('Player 2 wins')
+        fprintf ('%s wins',P2name)
     end
     
 
 % Check diag for P1 win
 
     if i==1 && sum (diag(Dboard)==P1const*ones(3,1))==3 
-        disp('Player 1 wins')
+        fprintf ('%s wins',P1name)
         
     end  
  
     if Dboard(1,3) == 88 && Dboard (2,2)==88 && Dboard (3,1) ==88
-        disp ('Player 1 wins')
+        fprintf ('%s wins',P1name)
     end
     
  % Check diag for P2 win
 
     if i==1 && sum (diag(Dboard)==P2const*ones(3,1))==3 
-        disp('Player 2 wins')
+        fprintf ('%s wins',P2name)
     end  
  
     if Dboard(1,3) == 79 && Dboard (2,2)==79 && Dboard (3,1) ==79
-        disp ('Player 2 wins')
+        fprintf ('%s wins',P2name)
     end
     
 
