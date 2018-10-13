c2b917fc5227164d219298c1027aa5de969ae04a
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-10-13 19:42:05 -0400

    ?

--- Ini.m
+++ Ini.m
@@ -3,7 +3,7 @@ repeat = true;
 while(repeat == true)
     P1name = input("Player one input your name:\n", "s");
     P2name = input("Player two input your name:\n", "s");
-    Display
+    DisplayG
     play = input("Want to play again?(Y for yes, any other key for no)\n", 's');
     if (play == "y")
         repeat = true;
