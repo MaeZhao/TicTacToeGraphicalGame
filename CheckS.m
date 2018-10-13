efb3b3d0090b1a5f32478a3be17887766a7a294d
Clorthe44 <44092778+Clorthe44@users.noreply.github.com>
2018-10-13 18:58:53 -0400

    CheckS
    CheckS.m

---
+++ CheckS.m
@@ -0,0 +1,28 @@
+%CheckS
+if Boardm(row,col)==0
+    available=true
+    if counter==1 
+        Boardm(row,col)=88;
+    else
+        Boardm(row,col)=79;
+    end
+    
+    else disp('pick again')
+        counter=1
+        end 
+   
+  
+   
+    
+ 
+   
+   
+    
+        
+        
+       
+    
+    
+    
+        
+  
