d5608027bde96b2dac0dbe59f5475b9585a2f2d4
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-10-13 18:57:58 -0400

    Add files via upload

--- Display.m
+++ Display.m
@@ -1,6 +1,27 @@
-%% Displays graphical version of TicTacToe
 clear
 clc
+%%CHANGE VARIABLE VALUES
 load TicTacToe
 figure('WindowStyle', 'docked');
 imshow([Board{1,:};Board{2,:};Board{3,:}]);
+counter = false; %temp
+stop = false; %temp
+while (stop==false)
+    if mod(counter, 2) == 0
+        [x,y] = ginput(1);
+        row = floor(y/123)+1;
+        col = floor(x/138)+1;
+        CheckS
+        Board {row, col} = X;
+        counter = counter + 1;
+    else
+        [x,y] = ginput(1);
+        row = floor(y/123)+1;
+        col = floor(x/138)+1;
+        CheckS
+        Board {row, col} = O;
+        counter = counter + 1;
+    end
+    imshow([Board{1,:};Board{2,:};Board{3,:}]); %% MUST USE THIS TO UPDATE
+    CheckWin
+end
