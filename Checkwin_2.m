c6b5bf41b0cbb5472608712fcc96d5abf1208045
Brianna <brianna.raphino@gmail.com>
2018-10-15 08:55:39 -0400

    Update Checkwin_2.m
    Dboard to DBoard

--- Checkwin_2.m
+++ Checkwin_2.m
@@ -1,7 +1,7 @@
 % Check row for P1 win
        
 for i = 1:3
-    if sum(Dboard(i,:)==P1const*ones (1,3))== 3
+    if sum(DBoard(i,:)==P1const*ones (1,3))== 3
        
         P1win = 1;
         fprintf ('%s wins',P1name)
@@ -10,7 +10,7 @@ for i = 1:3
 
 % Check columns for P1 win
 
-    if sum(Dboard(:,1)==P1const*ones (1,3))== 3
+    if sum(DBoard(:,1)==P1const*ones (1,3))== 3
        
         P1win = 1;
        fprintf ('%s wins', P1name)
@@ -20,7 +20,7 @@ for i = 1:3
 
 % Check row for P2 win
 
-    if sum(Dboard(i,:)==P2const*ones (1,3))== 3
+    if sum(DBoard(i,:)==P2const*ones (1,3))== 3
        
         P2win = 1;
         fprintf ('%s wins',P2name)
@@ -30,7 +30,7 @@ for i = 1:3
 
 % Check columns for P2 win
 
-    if sum(Dboard(:,1)==P2const*ones (1,3))== 3
+    if sum(DBoard(:,1)==P2const*ones (1,3))== 3
        
         P2win = 1;
         fprintf ('%s wins',P2name)
@@ -39,22 +39,22 @@ for i = 1:3
 
 % Check diag for P1 win
 
-    if i==1 && sum (diag(Dboard)==P1const*ones(3,1))==3 
+    if i==1 && sum (diag(DBoard)==P1const*ones(3,1))==3 
         fprintf ('%s wins',P1name)
         
     end  
  
-    if Dboard(1,3) == 88 && Dboard (2,2)==88 && Dboard (3,1) ==88
+    if DBoard(1,3) == 88 && DBoard (2,2)==88 && DBoard (3,1) ==88
         fprintf ('%s wins',P1name)
     end
     
  % Check diag for P2 win
 
-    if i==1 && sum (diag(Dboard)==P2const*ones(3,1))==3 
+    if i==1 && sum (diag(DBoard)==P2const*ones(3,1))==3 
         fprintf ('%s wins',P2name)
     end  
  
-    if Dboard(1,3) == 79 && Dboard (2,2)==79 && Dboard (3,1) ==79
+    if DBoard(1,3) == 79 && DBoard (2,2)==79 && DBoard (3,1) ==79
         fprintf ('%s wins',P2name)
     end
     
