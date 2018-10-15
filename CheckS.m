49615615d649aa8ca823fc59fe8f86f5785fb2f7
Brianna <brianna.raphino@gmail.com>
2018-10-15 18:43:34 -0400

    Caldwell Updates
    CheckS: change from counter == 1 to mod(counter, 2) == 0
    Ini: add clear clc and loadVariables

--- CheckS.m
+++ CheckS.m
@@ -1,7 +1,7 @@
 %CheckS
 if DBoard(row,col)==0
     available=true
-    if counter==1 
+    if mod(counter, 2) == 0 %change from counter == 1 to mod(counter, 2) == 0
         DBoard(row,col)=88;
     else
         DBoard(row,col)=79;
