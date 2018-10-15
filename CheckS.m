d94a85ff9f58df7fcc47cdc4ba4172451110a642
Mae Zhao <39135372+MaeZhao@users.noreply.github.com>
2018-10-15 18:47:29 -0400

    Update CheckS.m

--- CheckS.m
+++ CheckS.m
@@ -1,12 +1,10 @@
 %CheckS
 if DBoard(row,col)==0
-<<<<<<< HEAD
     available=true
     if mod(counter, 2) == 0 %change from counter == 1 to mod(counter, 2) == 0
-=======
-    available= true
+        available= true
+    end
     if mod(counter, 2) == 0
->>>>>>> 64569c4011d0fe53996bb3e3ea700c546427317d
         DBoard(row,col)=88;
     else
         DBoard(row,col)=79;
