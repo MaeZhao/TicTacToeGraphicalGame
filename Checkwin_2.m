b51be52e9faa3f73c644c7a9ac3323857696d311
Brianna <brianna.raphino@gmail.com>
2018-10-15 19:42:26 -0400

    Add Semicolons

--- Checkwin_2.m
+++ Checkwin_2.m
@@ -42,29 +42,29 @@ for i = 1:3
 % Check diag for P1 win
 
     if i==1 && sum (diag(DBoard)==P1const*ones(3,1))==3 
-        P1win =P1win + 1
+        P1win =P1win + 1;
         fprintf ('%s wins',P1name)
         
     end  
  
     if DBoard(1,3) == 88 && DBoard (2,2)==88 && DBoard (3,1) ==88
-        P1win =P1win + 1
+        P1win =P1win + 1;
         fprintf ('%s wins',P1name)
     end
     
  % Check diag for P2 win
 
     if i==1 && sum (diag(DBoard)==P2const*ones(3,1))==3 
-        P2win =P2win + 1
+        P2win =P2win + 1;
         fprintf ('%s wins',P2name)
     end  
  
     if DBoard(1,3) == 79 && DBoard (2,2)==79 && DBoard (3,1) ==79
-        P2win =P2win + 1
-        fprintf ('%s wins',P2name)
+        P2win =P2win + 1;
+        fprintf ('%s wins',P2name);
     end
     if winCount < (P1win + P2win)
-        gameEnd = true
+        gameEnd = true;
         Ini
     end
 end
--- Ini.m
+++ Ini.m
@@ -1,5 +1,5 @@
 %% Checks whether characters have won, lost or at a tie
-if gameEnd == false
+if gameEnd == false;
     play = input("Want to play a game?(y for yes, any other key for no)\n", 's');
     if (play == "y")
         P1name = input("Player one input your name:\n", "s");
