4ce87926c41973273a1995a25773ed061ba697e7
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-10-15 20:00:58 -0400

    Update Checkwin_2.m

--- Checkwin_2.m
+++ Checkwin_2.m
@@ -1,12 +1,16 @@
 % Check row for P1 win
 winCount = P1win + P2win;     
+% Check tie
+if counter >= 9
+    fprintf ('%s %s is at a tie ',P1name, P2name)  
+    gameEnd = true;
+    Ini
+end
 for i = 1:3
     if sum(DBoard(i,:)==P1const*ones (1,3))== 3
        
         P1win = P1win+1;
-        fprintf ('%s wins',P1name)
-       
-        
+        fprintf ('%s wins',P1name)   
     end
      
 
@@ -63,6 +67,7 @@ for i = 1:3
         P2win =P2win + 1;
         fprintf ('%s wins',P2name);
     end
+% Check win
     if winCount < (P1win + P2win)
         gameEnd = true;
         Ini
